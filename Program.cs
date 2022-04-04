6157bb9bee18ab3816175c497fa6066181c42a13
CodeAngel <83538964+CodeAngel3@users.noreply.github.com>
2022-04-05 00:32:39 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -13,16 +13,22 @@ namespace RipperStoreAutoInstaller
         public static void Main(string[] args)
         {
             string steamPath = null;
+            Console.WriteLine("Attempting to find VRChat install path... (Method 1)");
             var b32 = Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Valve\Steam", "InstallPath", null);
             if (b32 == null)
             {
+
+                Console.WriteLine("Failed to find VRChat install path. (Method 1)");
+                Console.WriteLine("Attempting to find VRChat install path... (Method 2)");
                 var b64 = Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Valve\Steam", "InstallPath", null);
                 if (b64 == null)
                 {
+                    Console.WriteLine("Failed to find VRChat install path. (Method 2)");
+                    Console.WriteLine("Attempting to find VRChat install path... (Method 3)");
                     var alt = Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Valve\Steam", "SteamPath", null);
                     if (alt == null)
                     {
-                        MessageBox.Show("Could not find Steam install path. Please select the path manually.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        MessageBox.Show("Could not find VRChat install path automatically. Please select the path manually.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
 
                         FileDialog();
                     }
@@ -31,6 +37,7 @@ namespace RipperStoreAutoInstaller
                 else steamPath = b64.ToString();
             }
             else steamPath = b32.ToString();
+
 
             if (!string.IsNullOrEmpty(steamPath))
             {
@@ -44,7 +51,7 @@ namespace RipperStoreAutoInstaller
                 }
                 else
                 {
-                    Install(appPath);
+                    Install(Path.Combine(appPath, "VRChat.exe"));
                 }
             }
             else
@@ -55,6 +62,7 @@ namespace RipperStoreAutoInstaller
 
         private static void FileDialog()
         {
+            Application.EnableVisualStyles();
             OpenFileDialog ofd = new OpenFileDialog();
             ofd.Title = "Please navigate to the game install directory and select VRChat.exe";
             ofd.Filter = "VRChat (*.exe)|*.exe";
@@ -70,6 +78,7 @@ namespace RipperStoreAutoInstaller
 
         private static void Install(string path)
         {
+            Console.WriteLine("Installing to " + path);
             string dir = Path.GetDirectoryName(path);
             string file = Path.GetFileName(path);
             FileInfo fi = new FileInfo(path);
@@ -78,27 +87,29 @@ namespace RipperStoreAutoInstaller
             {
                 using (WebClient c = new WebClient())
                 {
+
+                    Console.WriteLine("Downloading MelonLoader...");
                     c.DownloadFile("https://github.com/LavaGang/MelonLoader/releases/latest/download/MelonLoader.x64.zip", fi.Directory.FullName + "\\MelonLoader.x64.zip");
 
                     try
                     {
+                        Console.WriteLine("Extracting MelonLoader...");
                         ZipArchive zipArchive = ZipFile.OpenRead(fi.Directory.FullName + "\\MelonLoader.x64.zip");
                         foreach (ZipArchiveEntry entry in zipArchive.Entries) { entry.ExtractToFile(fi.Directory.FullName + entry.Name, true); }
                         zipArchive.Dispose();
                     }
                     catch { }
 
-                    try { File.Delete(fi.Directory.FullName + "\\MelonLoader.x64.zip"); } catch { }
+                    try { Console.WriteLine("Removing MelonLoader Temp files..."); ; File.Delete(fi.Directory.FullName + "\\MelonLoader.x64.zip"); } catch { }
 
+
+                    Console.WriteLine("Downloading Mod...");
+                    Directory.CreateDirectory(fi.Directory.FullName + "\\Mods");
                     c.DownloadFile("https://github.com/CodeAngel3/RipperStoreCredits/releases/latest/download/RipperStoreCredits.dll", fi.Directory.FullName + "\\Mods\\RipperStoreCredits.dll");
-
-                    MessageBox.Show("Successfully installed, launch vrchat to proceed.", "Success", MessageBoxButtons.OK, MessageBoxIcon.None);
+                    Console.WriteLine("\n----\nDone!\nYou can now launch VRChat and use the RipperStoreCredits mod.\nDon't forget to enter your Ripper.Store API key when prompted during the first launch.");
+                    Console.WriteLine("\nPress any key to exit...");
+                    Console.ReadKey();
                 }
-            }
-            else
-            {
-                MessageBox.Show("Invalid .exe detected, Please select your 'VRChat.exe'", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                FileDialog();
             }
         }
     }
